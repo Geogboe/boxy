7498b1614eaef1ce1a31f708136fbba58ac73c7e
geogboe <3868453+geogboe@users.noreply.github.com>
2025-11-24 14:40:26 -0500

    chore: add tools.go for managing development dependencies
    This commit introduces a new tools.go file that includes various development tools as dependencies. The tools listed include golangci-lint for linting, goimports for formatting, govulncheck for vulnerability scanning, and the gRPC and protobuf code generators.

---
+++ tools.go
@@ -0,0 +1,12 @@
+//go:build tools
+// +build tools
+
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/vuln/cmd/govulncheck"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
